3343de06c029ed285e8bb36de1887b35d44d3e91
Gunnar Pietz <77841571+gpietz@users.noreply.github.com>
2022-04-03 06:00:00 +0200

    Added input handling system.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
 extern crate bevy_ecs;
 extern crate raylib;
 
-use bevy_ecs::prelude::{Schedule, Stage, SystemStage, World};
+use std::collections::hash_map::Keys;
+use bevy_ecs::prelude::{Query, Schedule, Stage, SystemStage, World, Component};
 use bevy_ecs::system::{NonSend, ResMut};
-use raylib::{RaylibHandle, RaylibThread};
 use raylib::color::Color;
 use raylib::drawing::RaylibDraw;
+use raylib::{RaylibHandle, RaylibThread};
+use raylib::consts::KeyboardKey::*;
 
 const WIDTH: i32 = 800;
 const HEIGHT: i32 = 600;
 
+#[derive(Component)]
+struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
 fn main() {
-    let (raylib_handle, raylib_thread) = raylib::init()
+    let (mut raylib_handle, raylib_thread) = raylib::init()
         .size(WIDTH, HEIGHT)
         .title("Raylib Bevy ECS")
         .build();
+    raylib_handle.set_target_fps(60);
 
     let mut world = World::new();
     world.insert_resource(raylib_handle);
     world.insert_non_send(raylib_thread);
+    world.spawn().insert(Position { x: 100, y: 100 });
 
     let mut schedule = Schedule::default();
-    schedule.add_stage("render", SystemStage::single_threaded().with_system(render));
+    schedule.add_stage(
+        "render",
+        SystemStage::single_threaded()
+            .with_system(input)
+            .with_system(render),
+    );
 
     loop {
         if should_close(&mut world) {
@@ -38,8 +53,34 @@ fn should_close(world: &mut World) -> bool {
         .window_should_close()
 }
 
-fn render(mut rl_handle: ResMut<RaylibHandle>, rl_thread: NonSend<RaylibThread>) {
+fn input(
+    mut rl_handle: ResMut<RaylibHandle>,
+    mut query: Query<&mut Position>
+) {
+    let mut position = query.single_mut();
+    if rl_handle.is_key_down(KEY_LEFT ) && position.x > 0 {
+        position.x -= 4;
+    }
+    if rl_handle.is_key_down(KEY_RIGHT ) && position.x + 40 < rl_handle.get_screen_width() {
+        position.x += 4;
+    }
+    if rl_handle.is_key_down(KEY_UP) && position.y > 0 {
+        position.y -= 4;
+    }
+    if rl_handle.is_key_down(KEY_DOWN) && position.y + 40 < rl_handle.get_screen_height() {
+        position.y += 4;
+    }
+}
+
+fn render(
+    mut rl_handle: ResMut<RaylibHandle>,
+    rl_thread: NonSend<RaylibThread>,
+    query: Query<&Position>
+) {
     let mut draw = rl_handle.begin_drawing(&rl_thread);
     draw.clear_background(Color::BLACK);
     draw.draw_fps(10, 10);
+
+    let position = query.single();
+    draw.draw_rectangle(position.x, position.y, 40, 40, Color::YELLOW);
 }
